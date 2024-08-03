affcdbacae32a936ea22ecfd314ef8773e2a6892
Aifedayo <akeemifedayolag@gmail.com>
2024-08-03 18:07:14 +0100

    Outlining the process of using percentile to remove outliers

--- exercise.py
+++ exercise.py
@@ -1,1 +1,28 @@
-###
+######################################
+##           EXERCISE               ##
+######################################
+
+"""
+Use this air bnb new york city data set and remove outliers using
+percentile based on price per night for a given apartment/home.
+You can use suitable upper and lower limits on percentile based
+on your intuition. Your goal is to come up with new pandas dataframe
+that doesn't have the outliers present in it.
+
+"""
+import os # For getting the source of the file
+import pandas as pd # Data processing and reading csv files
+import numpy as np # For linear algebra
+import matplotlib.pyplot as plt # For visualization
+
+
+df = pd.read_csv("/home/akeemlag/Downloads/archive-kaggle/AB_NYC_2019.csv")
+
+# df.info()
+
+# Visualize the data you have using .head()
+print(df.head())
+
+# Create a box plot to see the outliers using matplotlib
+
+
