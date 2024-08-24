9ee971b1581f407ca4d442232550e05c0a3b2ce1
Aifedayo <akeemifedayolag@gmail.com>
2024-08-24 08:08:53 +0100

    create the server funct route

--- Projects/flask server/server/server.py
+++ Projects/flask server/server/server.py
@@ -3,13 +3,26 @@ import util
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
-    response.headers.add('Access-Control-Allow-Origin')
-    return "Hi"
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = (request.form['location'])
+    BHK = float(request.form['BHK'])
+    bath = float(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_prices(location, total_sqft, BHK, bath)
+    })
+    return response
+
 
 
 if __name__ == '__main__':
--- Projects/flask server/server/util.py
+++ Projects/flask server/server/util.py
@@ -1,9 +1,29 @@
 import json
 import pickle
+import numpy as np
+import pandas as pd
 
 __locations = None
 __data_columns = None
 __model = None
+
+
+def get_estimated_prices(location, total_sqft, BHK, bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = total_sqft
+    x[1] = BHK
+    x[2] = bath
+    if loc_index >= 0:
+        x[loc_index] = 1
+    
+    x_df = pd.DataFrame([x], columns=__data_columns)
+    return round(__model.predict(x_df)[0], 2)
+
 
 def get_location_names():
     return __locations
@@ -18,11 +38,17 @@ def load_saved_artifacts():
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
+    global __model
     with open("../artifacts/house-pricing-model", "rb") as f:
         __model = pickle.load(f)
 
     print('Loading saved artifacts...done!')
 
+
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_prices('1st phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_prices('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_prices('Kalhalli', 1000, 2, 2))
+    print(get_estimated_prices('Ejipura', 1000, 2, 2))
