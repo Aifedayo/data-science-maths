5915146e5d694e8717c9e9983841193787dbd4cc
Aifedayo <akeemifedayolag@gmail.com>
2024-08-26 12:13:04 +0100

    Test run the server

--- Projects/flask_server_car_price_pred/server/server.py
+++ Projects/flask_server_car_price_pred/server/server.py
@@ -4,3 +4,13 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def get_home_page():
+    return 'Hello World!'
+
+
+
+if __name__ == '__main__':
+    print('Starting Python Flask Serve for Car Price Prediction...')
+    app.run()
