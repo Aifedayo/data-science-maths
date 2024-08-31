a6a4ada68a695e4d7f09fce6e7613c8ebd422c55
Aifedayo <akeemifedayolag@gmail.com>
2024-08-31 16:38:16 +0100

    move file from model to artifacts folder

---
+++ Projects/flask_server_car_price_pred_v2/server/utils.py
@@ -0,0 +1,19 @@
+import json
+import joblib
+import numpy as np
+import pandas as pd
+
+__data_columns = None
+__model_xgb = None
+__model_rfr = None
+
+__manufacturers_to_models_columns = None
+__manufacturers_columns = None
+__models_columns = None
+__categories_columns = None
+__colors_columns = None
+__gear_box_types_columns = None
+__fuel_types_columns = None
+__drive_wheels_columns = None
+
+
